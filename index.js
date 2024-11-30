3f213095505acd6014aef09806b30d0b564c11e1
tsamantanis <philip.tsamantanis@icloud.com>
2024-11-30 14:16:50 +0200

    add colors enpoint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');  // Importing cors
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 
@@ -13,11 +13,32 @@ app.use(cors());
 const quotesFilePath = path.join(__dirname, 'quotes.json');
 const quotes = JSON.parse(fs.readFileSync(quotesFilePath, 'utf-8'));
 
+// List of random colors
+const colors = [
+  "#F6E4D0", // Soft peach
+  "#F7B3A0", // Soft coral
+  "#E0C1D1", // Muted lavender
+  "#D46B3B", // Muted terracotta
+  "#9B4D28", // Deep rust
+  "#4B3F33", // Dark brown
+  "#4A4A48", // Charcoal gray
+  "#F1C6A1", // Warm light beige
+  "#D8A7A2", // Dusty rose
+  "#C2A0C0"  // Soft mauve
+];
+
 // Endpoint to get a random quote
 app.get('/quote', (req, res) => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
   res.json(randomQuote);
+});
+
+// Endpoint to get a random color
+app.get('/color', (req, res) => {
+  const randomColorIndex = Math.floor(Math.random() * colors.length);
+  const randomColor = colors[randomColorIndex];
+  res.json({ color: randomColor });
 });
 
 // Start the server
